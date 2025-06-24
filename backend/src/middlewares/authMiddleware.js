b595c39bd87ed57aaa7d4efa881c50c8c7a1006f
Awais12xd <lucifer.awaisxx213@example.com>
2025-06-24 16:23:52 +0500

    testing 2

--- backend/src/middlewares/authMiddleware.js
+++ backend/src/middlewares/authMiddleware.js
@@ -4,7 +4,7 @@ import { User } from "../models/User.model.js"
 
 export const verifyToken = async (req, _, next) => {
   const token =   req.cookies?.access_token  || req.headers.authorization?.split(" ")[1];
-  console.log(token, access_token)
+  console.log(token, "access_token")
   if (!token) {
     throw new ApiError(404, "Unauthorized error no token");
   }
