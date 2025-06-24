bcfd315189e965db54409f0c9cf916c7b007798b
Awais12xd <lucifer.awaisxx213@example.com>
2025-06-24 11:16:16 +0500

    Auth Changes

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -22,10 +22,10 @@ const authController = async (req, res, next) => {
       username: username.toLowerCase(),
       email,
       password,
-       avatar: {
-      url: "https://up.yimg.com/ib/th?id=OIP.hGSCbXlcOjL_9mmzerqAbQHaHa&pid=Api&rs=1&c=1&qlt=95&w=114&h=114",
-      public_id: null
-  }
+      avatar: {
+        url: "https://up.yimg.com/ib/th?id=OIP.hGSCbXlcOjL_9mmzerqAbQHaHa&pid=Api&rs=1&c=1&qlt=95&w=114&h=114",
+        public_id: null,
+      },
     });
 
     if (!user) {
@@ -41,13 +41,11 @@ const authController = async (req, res, next) => {
 };
 
 const loginController = async (req, res, next) => {
-
   try {
     const { email, password } = req.body;
     if (!email || !password) {
       return next(new ApiError(400, "Please fill all the fields"));
     }
-
     const user = await User.findOne({ email });
 
     if (!user) {
@@ -59,69 +57,85 @@ const loginController = async (req, res, next) => {
     if (!isPasswordValid) {
       return next(new ApiError(401, "Invalid credentials"));
     }
-     let { password: userPassword, ...rest } = user._doc;
+    let { password: userPassword, ...rest } = user._doc;
 
     const token = await user.generateToken();
-     const loggedInUser = await User.findById(user._id)
-     if(!loggedInUser){
+    const loggedInUser = await User.findById(user._id);
+    if (!loggedInUser) {
       return next(new ApiError(404, "User not found in logging"));
-      }
+    }
 
     return res
       .status(200)
-      .cookie("access_token" , token , {httpOnly : true})
+      .cookie("access_token", token, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "none",
+      })
       .json(new apiResponse(200, rest, "Login successful"));
   } catch (error) {
     next(error);
   }
-}
+};
 
 const googleController = async (req, res, next) => {
-try {
-  const user = await User.findOne({ email: req.body.email }); ;
-  
-  if (user) {
-    const token = await user.generateToken();
-    let { password: userPassword, ...rest } = user._doc;
-    return res
-      .status(200)
-      .cookie("access_token" , token , {httpOnly : true})
-      .json(new apiResponse(200, rest, "Login successful"));
-  } else {
-    const generatedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
-    const hashedPassword= await bcrypt.hash(generatedPassword,10);
-    const newUser = await User.create({
-      username: req.body.username.trim().toLowerCase().replace(/\s+/g, "") + Math.random().toString(36).substring(2, 6),
-     email: req.body.email,
-      password: hashedPassword,
-      avatar:req.body.photo
-    });
-    if (!newUser) {
-      return next(new ApiError(500, "User creation failed"));
-    }
-    const token = await newUser.generateToken();
-    let { password: userPassword, ...rest } = newUser._doc;
-    return res
-    .status(200)
-    .cookie("access_token" , token , {httpOnly : true})
-    .json(new apiResponse(200, rest, "User created and logged in successfully"));
-  }
+  try {
+    const user = await User.findOne({ email: req.body.email });
 
+    if (user) {
+      const token = await user.generateToken();
+      let { password: userPassword, ...rest } = user._doc;
+      return res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+          secure: true,
+          sameSite: "none",
+        })
+        .json(new apiResponse(200, rest, "Login successful"));
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = await bcrypt.hash(generatedPassword, 10);
+      const newUser = await User.create({
+        username:
+          req.body.username.trim().toLowerCase().replace(/\s+/g, "") +
+          Math.random().toString(36).substring(2, 6),
+        email: req.body.email,
+        password: hashedPassword,
+        avatar: req.body.photo,
+      });
+      if (!newUser) {
+        return next(new ApiError(500, "User creation failed"));
+      }
+      const token = await newUser.generateToken();
+      let { password: userPassword, ...rest } = newUser._doc;
+      return res
+        .status(200)
+        .cookie("access_token", token, {
+          httpOnly: true,
+          secure: true, 
+          sameSite: "none",
+        })
+        .json(
+          new apiResponse(200, rest, "User created and logged in successfully")
+        );
+    }
   } catch (error) {
     next(error);
   }
-
-}
+};
 
 const signOutController = async (req, res, next) => {
   try {
     res.clearCookie("access_token");
-    return res.status(200).json(new apiResponse(200, null, "Logged out successfully"));
-    
+    return res
+      .status(200)
+      .json(new apiResponse(200, null, "Logged out successfully"));
   } catch (error) {
     next(error);
   }
-}
+};
 
-
-export { authController , loginController , googleController , signOutController};
+export { authController, loginController, googleController, signOutController };
