00d35ae4b405efa1d720001ea872db738a88a88e
Awais12xd <lucifer.awaisxx213@example.com>
2025-06-24 15:48:09 +0500

    add new url in cors

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://real-estate-roan-one.vercel.app',          
+  origin: ['https://real-estate-roan-one.vercel.app',
+    'http://localhost:5173'
+  ],          
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials : true
