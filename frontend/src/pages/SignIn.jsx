1766873423def4003dfefe328dccf1b59b6b4733
Awais12xd <lucifer.awaisxx213@example.com>
2025-06-24 16:20:58 +0500

    add true in credentials of sign in

--- frontend/src/pages/SignIn.jsx
+++ frontend/src/pages/SignIn.jsx
@@ -32,6 +32,7 @@ const SignIn = () => {
           "Content-Type": "application/json",
           },
           body: JSON.stringify(formData),
+          credentials: "include"
       }
     )
     const data = await res.json();
