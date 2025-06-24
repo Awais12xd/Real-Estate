d04a873679e6c0c9664e71deaa2c4027ee129a5e
Awais12xd <lucifer.awaisxx213@example.com>
2025-06-24 15:38:31 +0500

    Remove the / from url which cause the cors error

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'https://real-estate-roan-one.vercel.app/',          
+  origin: 'https://real-estate-roan-one.vercel.app',          
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials : true
