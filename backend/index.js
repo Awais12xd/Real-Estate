e5dcc2ca405a958c59f40d3476d59cfd96074d98
Awais12xd <lucifer.awaisxx213@example.com>
2025-06-24 15:31:16 +0500

    update th e frontend url

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(cors({
-  origin: 'http://localhost:5173',          
+  origin: 'https://real-estate-roan-one.vercel.app/',          
   methods: ["GET","POST","PUT","DELETE","OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials : true
