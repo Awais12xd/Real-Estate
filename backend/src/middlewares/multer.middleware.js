f73c639274fddee87ee8538aba08760f3e1a7fb1
Awais12xd <lucifer.awaisxx213@example.com>
2025-06-24 16:48:42 +0500

    setup multer

--- backend/src/middlewares/multer.middleware.js
+++ backend/src/middlewares/multer.middleware.js
@@ -1,5 +1,12 @@
 import multer from "multer";
+import fs from 'fs';
+import path from 'path';
 
+
+const tempDir = path.join(process.cwd(), 'public', 'temp');
+if (!fs.existsSync(tempDir)) {
+  fs.mkdirSync(tempDir, { recursive: true }); // creates the directory if it doesn't already exist
+}
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
