44e1e3bf6bc5df18e98362b74acf3fc7237b661b
Awais12xd <lucifer.awaisxx213@example.com>
2025-12-19 20:59:18 +0500

    Do changes

--- backend/index.js
+++ backend/index.js
@@ -15,17 +15,15 @@ const app = express();
 
 dotenv.config();
 
+app.use(cors({
+  origin: ['https://real-estate-roan-one.vercel.app','http://localhost:5173'],          
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin: ['https://real-estate-roan-one.vercel.app',
-    'http://localhost:5173'
-  ],          
-  methods: ["GET","POST","PUT","DELETE","OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials : true
-}));
 
 
 const PORT = process.env.PORT || 4000;
